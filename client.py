7b7c282d5a0937246809ee2b3f3aa062ce04403f
veggiebob <veggiebobowman@gmail.com>
2020-03-21 14:58:22 -0600

    first commit

---
+++ client.py
@@ -0,0 +1,193 @@
+import socket, types, time, threading, os, configparser
+import sys
+
+from handlers import *
+
+# https://modern.ircdocs.horse/
+
+
+class Navigation:
+    def __init__(self, current_channel: str = ''):
+        self.current_channel = current_channel
+
+class Server:
+    def __init__(self, host: str, port: int, username: str, nickname: str, password: str = '', hostname: str = ''):
+        self.hostname = hostname
+        self.host = host
+        self.port = int(port)
+        self.password = password
+        self.nickname = nickname
+        self.username = username
+
+        self.sock: socket.socket = None
+        self.listen_thread: threading.Thread = None
+        self.connected = False
+        self.registered = False
+        self.listening = False
+
+    def connect(self) -> bool:  # successful?
+        start_time, end_time = 0, 0
+        try:
+            self.sock = socket.socket()
+            start_time = time.perf_counter()
+            self.sock.connect((self.host, self.port))
+            end_time = time.perf_counter()
+            self.connected = True
+            print('connected in %ss'%(end_time-start_time))
+            self.on_successful_connect()
+            return True
+        except (ConnectionError, TimeoutError) as e:
+            end_time = time.perf_counter()
+            print('failed to connect (%ss) to %s at %s' % (end_time-start_time, self.hostname, self.host))
+            return False
+
+    def on_successful_connect(self) -> None:
+        self.register()
+        self.start_listen()
+
+    def start_listen(self) -> None:
+        self.listen_thread = threading.Thread(target=self.continuous_listen)
+        self.listen_thread.start()
+        self.listening = True
+
+    def continuous_listen(self):
+        while True:
+            message = self.get_raw_message(1024) # hm
+            response:ResponseHandler = ResponseHandler.parse_response(message)
+            ResponseHandler.handle_response(response, self)
+            if not self.listening:
+                break
+
+    def stop_listen(self):
+        self.listening = False
+        self.listen_thread.join()
+
+    def get_raw_message(self, timeout: float = 1.0) -> str:
+        response = b""
+        starttime = time.perf_counter()
+        last = ''
+        while True:
+            latest = self.sock.recv(1)
+            if last == b'\r' and latest == b'\n':
+                # print('BREAKING BECAUSE OF \\r\\n')
+                response = response[:-1]
+                break
+            response += latest
+            timepassed = time.perf_counter() - starttime
+            if timepassed > timeout:
+                print('timeout of %s passed with time %s' % (timeout, timepassed))
+                return ''
+            last = latest
+        response = response.decode(encoding='UTF-8')
+        # print('got a string %s'%(str(response)))
+        return response
+
+    def stream_input(self, nav: Navigation):
+        print('type "exit" to exit streaming')
+        while True:
+            inn = input(f'{nav.current_channel} > ')
+            if inn.rstrip() == 'exit':
+                print('exiting . . .')
+                break
+            self.send_message(inn, nav)
+
+    def send_command(self, command: str, args: list = (), contents: str = '') -> None:
+        a = ' '.join([str(i) for i in args])
+        if len(args) > 0:
+            a = ' ' + a
+        if contents is '':
+            self.send_str('%s%s' % (command, a))
+        else:
+            self.send_str('%s%s :%s' % (command, a, contents))
+
+    def send_message(self, message: str, nav: Navigation) -> None:
+        self.send_command('PRIVMSG', ['#%s' % nav.current_channel], contents=message)
+
+    def send_str(self, message: str):
+        if self.connected:
+            self.sock.send(bytes(message + "\r\n", encoding='UTF-8'))
+
+    # def send_with_response(self, message: str, timeout=1) -> CommandResponse:
+    #     if self.connected:
+    #         self.send_str(message)
+    #         response = self.get_raw_message(timeout)
+    #         print('response is %s' % response)
+    #         return cmdR
+    #     else:
+    #         return CommandResponse()
+
+    def register(self):
+        # https://modern.ircdocs.horse/ > "Connection Registration"
+        # https://tools.ietf.org/html/rfc2812#section-3.1
+        self.send_str('CAP LS 302')
+        if self.password != '':
+            self.send_str('PASS %s' % self.password)
+        self.send_str('NICK %s' % self.nickname + '\r\nUSER %s 0 * :%s' % (self.username, self.username))
+        self.registered = True
+
+    def disconnect(self):
+        if self.listening:
+            self.stop_listen()
+        self.sock.close()
+        self.connected = False
+        self.registered = False
+
+    def ready(self):
+        return self.connected and self.registered and self.listening
+
+    def status(self):
+        return 'Connected: %s\nRegistered: %s\nListening: %s' % (self.connected, self.registered, self.listening)
+
+    def __str__(self) -> str:
+        return \
+            f"""
+        Server {self.hostname} at {self.host} on port {self.port}
+        Username: {self.username}, Nick: {self.nickname}
+        {self.status()}
+        """
+
+def get_config() -> dict:
+    try:
+        config_parser = configparser.ConfigParser()
+        config_parser.read('config/servers.ini', encoding="UTF-8")
+    except:
+        print('you must have a config/servers.ini file')
+        return {}
+    out = {}
+    for server in config_parser.sections():
+        server_name = str(server)
+        out[server_name] = {}
+        for key in config_parser[server_name]:
+            value = config_parser[server_name][key]
+            if key=='port':
+                value = int(value)
+            out[server_name][key] = value
+    return out
+
+
+if __name__ == '__main__':
+    all_servers = get_config()
+    if len(all_servers.keys()) == 0:
+        print('no servers!')
+        sys.exit()
+    host_name = input('enter server name from any of [%s]:\n'%(', '.join([s for s in all_servers.keys()])))
+    try:
+        serv = all_servers[host_name]
+        serv['hostname'] = host_name
+    except:
+        print('unable to find data on host %s'%host_name)
+        sys.exit()
+    current_server = Server(**serv) # needs host, port, username and nickname
+    me = Navigation('')
+    print('connecting . . .')
+    current_server.connect()
+    if current_server.ready():
+        print('logged in as %s (%s)' % (current_server.nickname, current_server.username))
+        current_server.send_command('JOIN', ['#general'])
+        me.current_channel = 'general'
+        current_server.send_message('hello', me)
+        current_server.stream_input(me)
+        current_server.disconnect()
+    else:
+        print('server status: ')
+        print(current_server.status())
---
+++ handlers.py
@@ -0,0 +1,120 @@
+from typing import Callable, List
+
+class Command:
+    from client import Server
+    def __init__(self, command: str, callback: Callable[['ResponseHandler', Server], None]):
+        self.callback = callback
+        self.command_name = command
+
+    def call_command(self, response: 'ResponseHandler', server: Server) -> None:
+        self.callback(response, server)
+
+    def call_safe(self, response: 'ResponseHandler', server: Server) -> bool:  # whether or not it was called
+        if response.command == self.command_name:
+            self.call_command(response, server)
+            return True
+        return False
+
+    COMMANDS = []
+
+Command.COMMANDS = [
+    #Command('PING', lambda r, s: s.send_command('PONG')) # hm not sure if these will be used
+]
+
+class CommandResponse:
+    from client import Server
+    def __init__(self, number: int = 0):
+        self.number = number
+
+    def handle_response(self, response: 'ResponseHandler', server: Server) -> None:
+        # https://tools.ietf.org/html/rfc2812#section-5
+        if self.number == 432:  # bad nickname
+            print('bad nick!')
+        elif self.number == 1:  # welcome
+            print(response.content)
+        elif self.number == 2:  # another welcome message?
+            print(response.content)
+        elif self.number == 352:  # a general message to be logged
+            user: str = response.parameters[1]
+            channel: str = response.parameters[0]
+            print('%s > %s' % (user, response.content))
+        else:
+            print(response.content)
+
+    def __str__(self):
+        return 'CommandResponse<response code: %d>' % self.number
+
+
+class ResponseHandler:
+    from client import Server
+    @staticmethod
+    def parse_response(response: str) -> 'ResponseHandler':
+        parts = response.split(' ')
+        for p in parts:
+            if p is '':
+                parts.remove(p)
+        if len(parts) == 0:
+            print('%s is a BAD RESPONSE!!!!' % response)
+            return ResponseHandler()  # should have 0 of these
+        src = ''
+        cmd = ''
+        parameters = []
+        content, hcontent = '', False
+        p1 = -1
+        if parts[0][0] is ':':  # check for source
+            src = parts[0][1:]  # get rid of :
+            cmd = parts[1]
+            p1 = 2
+        else:
+            cmd = parts[0]
+            p1 = 1
+
+        if len(parts) >= p1:  # get parameters and content
+            for i in range(p1, len(parts)):
+                p: str = parts[i]
+                if p[0] is ':':
+                    hcontent = True
+                    p = p[1:] # get rid of colon
+                if hcontent:
+                    content += ' ' + p  # because content was separated by spaces
+                else:
+                    parameters.append(p)
+        return ResponseHandler(
+            source=src,
+            command=cmd.lower(),
+            parameters=parameters,
+            content=content
+        )
+
+    def handle_response(self, server: Server) -> bool:  # whether or not it was handled
+        # switch command
+        cmd = self.command
+        try:
+            cmd_num = int(cmd)
+            CommandResponse.handle_response(CommandResponse(cmd_num), self, server)
+            return True
+        except ValueError:
+            pass
+        for c in Command.COMMANDS:
+            if c.call_safe(self, server): return True
+        if cmd == 'ping':
+            server.send_command('PONG')
+            print('sending pong')
+        elif cmd == 'notice':
+            print('NOTICE: ' + self.content)
+        elif cmd == 'cap':
+            print('haha cap')
+            server.send_str('no')
+        else:
+            print('unable to parse %s or command %s'%(self, self.command))
+            return False
+
+    def __init__(self, **kwargs):  # remove standard marks
+        self.source: str = kwargs['source']  # without :, default ''
+        self.command: str = kwargs['command']  # lowercase
+        self.parameters: list = kwargs['parameters']  # list of strs without spaces, of course. if none, empty list
+        self.content: str = kwargs['content']  # without :, default ''
+
+    def __str__(self):
+        return f"ResponseHandler<source: {self.source}, command: {self.command}, parameters: {self.parameters}, content: {self.content}>"
+
